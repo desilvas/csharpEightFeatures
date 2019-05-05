72eb016fb1a4b70aad7a0eb22ab20e7c181ecc2f
shalinds <shalinds@gmail.com>
2019-05-05 22:12:15 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplicationdemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Index start = 3;  // Get the 4th element from the beginning
+            Index end = ^3;   // Get the 3rd element from the end
+
+            string[] numbers = { "Zero", "One", "Two", "Three", "Four", "Five", "Six" };
+
+            Console.WriteLine($"{numbers[start]}, {numbers[end]}"); // "Three , Four"
+
+
+            var range = numbers[1..^2];
+
+            foreach (var item in range)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+
